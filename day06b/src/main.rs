7f5925ddaa6ab3b22f4a27bae24a89d144be4c08
Andrew Howard <andrew@adh.me.uk>
2022-12-06 13:46:44 +0000

    Added day 6 part 2

---
+++ day06b/src/main.rs
@@ -0,0 +1,15 @@
+fn has_duplicates(chars: &&[char]) -> bool {
+    for (i, ch) in chars.iter().enumerate() {
+        if chars.iter().skip(i + 1).any(|x| x == ch) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+    let chars = input.chars().collect::<Vec<char>>();
+    let result = chars.windows(14).take_while(has_duplicates).count() + 14;
+    println!("{}", result);
+}
