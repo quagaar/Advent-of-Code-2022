e6599ce6a9d01306dc326aa65c883a813cae7281
Andrew Howard <andrew@adh.me.uk>
2022-12-09 13:54:12 +0000

    Added day 9 part 1

---
+++ day09a/src/main.rs
@@ -0,0 +1,73 @@
+use std::collections::HashSet;
+
+fn move_tail(head: &(i32, i32), tail: &mut (i32, i32)) {
+    match head.0 - tail.0 {
+        2 => {
+            tail.0 += 1;
+            tail.1 = head.1;
+        }
+        -2 => {
+            tail.0 -= 1;
+            tail.1 = head.1;
+        }
+        _ => (),
+    }
+    match head.1 - tail.1 {
+        2 => {
+            tail.1 += 1;
+            tail.0 = head.0;
+        }
+        -2 => {
+            tail.1 -= 1;
+            tail.0 = head.0;
+        }
+        _ => (),
+    }
+}
+
+fn main() {
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut head = (0, 0);
+    let mut tail = (0, 0);
+
+    for line in include_str!("input.txt").lines() {
+        let (direction, steps) = line.split_once(" ").unwrap();
+        let steps: i32 = steps.parse().unwrap();
+
+        match direction {
+            "U" => {
+                for _ in 0..steps {
+                    head.1 += 1;
+                    move_tail(&head, &mut tail);
+                    visited.insert(tail);
+                }
+            }
+            "D" => {
+                for _ in 0..steps {
+                    head.1 -= 1;
+                    move_tail(&head, &mut tail);
+                    visited.insert(tail);
+                }
+            }
+            "L" => {
+                for _ in 0..steps {
+                    head.0 += 1;
+                    move_tail(&head, &mut tail);
+                    visited.insert(tail);
+                }
+            }
+            "R" => {
+                for _ in 0..steps {
+                    head.0 -= 1;
+                    move_tail(&head, &mut tail);
+                    visited.insert(tail);
+                }
+            }
+            _ => panic!("Unknown direction: {}", line),
+        }
+    }
+
+    let result = visited.len();
+
+    println!("{}", result);
+}
