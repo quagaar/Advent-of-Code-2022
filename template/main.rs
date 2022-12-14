f4416e18c0452a301da8793de4acee3463d28ecd
Andrew Howard <andrew@adh.me.uk>
2022-12-14 13:18:57 +0000

    Added template for new days

---
+++ template/main.rs
@@ -0,0 +1,19 @@
+fn solve(input: &str) -> usize {
+    return input.len();
+}
+
+fn main() {
+    let result = solve(include_str!("input.txt"));
+    println!("{:?}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_result() {
+        let result = solve(include_str!("example.txt"));
+        assert_eq!(42, result);
+    }
+}
