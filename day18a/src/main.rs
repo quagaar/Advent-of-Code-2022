f48288e8badf8594e68cb74c49616145e0658652
Andrew Howard <andrew@adh.me.uk>
2022-12-18 14:15:53 +0000

    Improved performance using a hash set

--- day18a/src/main.rs
+++ day18a/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn parse_coordinate(line: &str) -> [i32; 3] {
     let parts = line
         .split(",")
@@ -6,24 +8,26 @@ fn parse_coordinate(line: &str) -> [i32; 3] {
     return [parts[0], parts[1], parts[2]];
 }
 
-fn is_adjacent(cube: &[i32; 3], other: &[i32; 3]) -> bool {
-    match (cube[0] - other[0], cube[1] - other[1], cube[2] - other[2]) {
-        (0, 0, 1) | (0, 0, -1) | (0, 1, 0) | (0, -1, 0) | (1, 0, 0) | (-1, 0, 0) => true,
-        _ => false,
-    }
+fn get_exposed_sides(cube: &[i32; 3], cubes: &HashSet<[i32; 3]>) -> usize {
+    (0..=2)
+        .flat_map(|axis| {
+            [(axis, 1), (axis, -1)]
+                .into_iter()
+                .filter(|(axis, direction)| {
+                    let mut pos = cube.clone();
+                    pos[*axis] += direction;
+                    !cubes.contains(&pos)
+                })
+        })
+        .count()
 }
 
 fn solve(input: &str) -> usize {
-    let cubes = input.lines().map(parse_coordinate).collect::<Vec<_>>();
+    let cubes = input.lines().map(parse_coordinate).collect::<HashSet<_>>();
 
     let result: usize = cubes
         .iter()
-        .map(|cube| {
-            6 - cubes
-                .iter()
-                .filter(|other| is_adjacent(cube, other))
-                .count()
-        })
+        .map(|cube| get_exposed_sides(cube, &cubes))
         .sum();
 
     return result;
