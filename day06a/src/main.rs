194d47687d2b04e2daf300df16b8ebcd9e7158ea
Andrew Howard <andrew@adh.me.uk>
2022-12-20 12:18:04 +0000

    Refactored day 6

--- day06a/src/main.rs
+++ day06a/src/main.rs
@@ -1,15 +1,51 @@
 fn has_duplicates(chars: &&[char]) -> bool {
-    for (i, ch) in chars.iter().enumerate() {
-        if chars.iter().skip(i + 1).any(|x| x == ch) {
-            return true;
-        }
-    }
-    return false;
+    chars
+        .iter()
+        .enumerate()
+        .any(|(i, ch)| chars.iter().skip(i + 1).any(|x| x == ch))
+}
+
+fn solve(input: &str) -> usize {
+    let chars = input.chars().collect::<Vec<char>>();
+    return chars.windows(4).take_while(has_duplicates).count() + 4;
 }
 
 fn main() {
-    let input = include_str!("input.txt");
-    let chars = input.chars().collect::<Vec<char>>();
-    let result = chars.windows(4).take_while(has_duplicates).count() + 4;
-    println!("{}", result);
+    let result = solve(include_str!("input.txt"));
+    println!("{:?}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1_result() {
+        let result = solve(include_str!("example1.txt"));
+        assert_eq!(7, result);
+    }
+
+    #[test]
+    fn example2_result() {
+        let result = solve(include_str!("example2.txt"));
+        assert_eq!(5, result);
+    }
+
+    #[test]
+    fn example3_result() {
+        let result = solve(include_str!("example3.txt"));
+        assert_eq!(6, result);
+    }
+
+    #[test]
+    fn example4_result() {
+        let result = solve(include_str!("example4.txt"));
+        assert_eq!(10, result);
+    }
+
+    #[test]
+    fn example5_result() {
+        let result = solve(include_str!("example5.txt"));
+        assert_eq!(11, result);
+    }
 }
--- day06b/src/main.rs
+++ day06b/src/main.rs
@@ -5,9 +5,47 @@ fn has_duplicates(chars: &&[char]) -> bool {
         .any(|(i, ch)| chars.iter().skip(i + 1).any(|x| x == ch))
 }
 
-fn main() {
-    let input = include_str!("input.txt");
+fn solve(input: &str) -> usize {
     let chars = input.chars().collect::<Vec<char>>();
-    let result = chars.windows(14).take_while(has_duplicates).count() + 14;
-    println!("{}", result);
+    return chars.windows(14).take_while(has_duplicates).count() + 14;
+}
+
+fn main() {
+    let result = solve(include_str!("input.txt"));
+    println!("{:?}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1_result() {
+        let result = solve(include_str!("example1.txt"));
+        assert_eq!(19, result);
+    }
+
+    #[test]
+    fn example2_result() {
+        let result = solve(include_str!("example2.txt"));
+        assert_eq!(23, result);
+    }
+
+    #[test]
+    fn example3_result() {
+        let result = solve(include_str!("example3.txt"));
+        assert_eq!(23, result);
+    }
+
+    #[test]
+    fn example4_result() {
+        let result = solve(include_str!("example4.txt"));
+        assert_eq!(29, result);
+    }
+
+    #[test]
+    fn example5_result() {
+        let result = solve(include_str!("example5.txt"));
+        assert_eq!(26, result);
+    }
 }
