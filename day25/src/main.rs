db7f581f89f324ce64b2e83808b625a1c29f3017
Andrew Howard <andrew@adh.me.uk>
2022-12-25 16:23:30 +0000

    More unit tests

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,25 +1,23 @@
-fn parse_snafu_number(line: &str) -> i64 {
-    line.chars()
-        .rev()
-        .fold((1, 0), |(multiple, mut total), ch| {
-            match ch {
-                '2' => total += 2 * multiple,
-                '1' => total += multiple,
-                '0' => {}
-                '-' => total -= multiple,
-                '=' => total -= 2 * multiple,
-                _ => panic!("Unknown digit {}", ch),
-            }
-            (multiple * 5, total)
-        })
-        .1
+fn parse_balanced_quinary(line: &str) -> i64 {
+    line.chars().fold(0, |mut acc, ch| {
+        acc *= 5;
+        match ch {
+            '2' => acc += 2,
+            '1' => acc += 1,
+            '0' => {}
+            '-' => acc -= 1,
+            '=' => acc -= 2,
+            _ => panic!("Unknown digit {}", ch),
+        }
+        acc
+    })
 }
 
-fn to_snafu_number(number: i64) -> String {
+fn to_balanced_quinary(number: i64) -> String {
     let mut digits = vec![];
     let mut remaining = number;
 
-    while remaining > 0 {
+    while remaining != 0 {
         match remaining % 5 {
             0 => digits.push('0'),
             1 => digits.push('1'),
@@ -32,17 +30,34 @@ fn to_snafu_number(number: i64) -> String {
                 digits.push('-');
                 remaining += 5;
             }
+            -1 => digits.push('-'),
+            -2 => digits.push('='),
+            -3 => {
+                digits.push('2');
+                remaining -= 5;
+            }
+            -4 => {
+                digits.push('1');
+                remaining -= 5;
+            }
             _ => panic!("Unexpected remainder: {}", remaining),
         }
         remaining /= 5;
     }
 
-    return String::from_iter(digits.into_iter().rev());
+    if digits.is_empty() {
+        return String::from("0");
+    } else {
+        return String::from_iter(digits.into_iter().rev());
+    }
 }
 
 fn solve(input: &str) -> String {
-    let numbers = input.lines().map(parse_snafu_number).collect::<Vec<_>>();
-    return to_snafu_number(numbers.into_iter().sum());
+    let numbers = input
+        .lines()
+        .map(parse_balanced_quinary)
+        .collect::<Vec<_>>();
+    return to_balanced_quinary(numbers.into_iter().sum());
 }
 
 fn main() {
@@ -53,10 +68,73 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
     #[test]
     fn example_result() {
         let result = solve(include_str!("example.txt"));
         assert_eq!("2=-1=0", result);
+    }
+
+    #[test]
+    fn puzzle_result() {
+        let result = solve(include_str!("input.txt"));
+        assert_eq!("20=212=1-12=200=00-1", result);
+    }
+
+    #[test_case("0" => 0)]
+    #[test_case("1" => 1)]
+    #[test_case("2" => 2)]
+    #[test_case("1=" => 3)]
+    #[test_case("1-" => 4)]
+    #[test_case("10" => 5)]
+    #[test_case("11" => 6)]
+    #[test_case("12" => 7)]
+    #[test_case("2=" => 8)]
+    #[test_case("2-" => 9)]
+    #[test_case("20" => 10)]
+    #[test_case("1=0" => 15)]
+    #[test_case("1-0" => 20)]
+    #[test_case("1=11-2" => 2022)]
+    #[test_case("1-0---0" => 12345)]
+    #[test_case("1121-1110-1=0" => 314159265)]
+    #[test_case("--=-1---01-20" => -314159265)]
+    fn test_parse_balanced_quinary(line: &str) -> i64 {
+        parse_balanced_quinary(line)
+    }
+
+    #[test_case(0 => "0")]
+    #[test_case(1 => "1")]
+    #[test_case(2 => "2")]
+    #[test_case(3 => "1=")]
+    #[test_case(4 => "1-")]
+    #[test_case(5 => "10")]
+    #[test_case(6 => "11")]
+    #[test_case(7 => "12")]
+    #[test_case(8 => "2=")]
+    #[test_case(9 => "2-")]
+    #[test_case(10 => "20")]
+    #[test_case(15 => "1=0")]
+    #[test_case(20 => "1-0")]
+    #[test_case(2022 => "1=11-2")]
+    #[test_case(12345 => "1-0---0")]
+    #[test_case(314159265 => "1121-1110-1=0")]
+    #[test_case(-1 => "-"; "convert minus 1")]
+    #[test_case(-2 => "="; "convert minus 2")]
+    #[test_case(-3 => "-2"; "convert minus 3")]
+    #[test_case(-4 => "-1"; "convert minus 4")]
+    #[test_case(-5 => "-0"; "convert minus 5")]
+    #[test_case(-6 => "--"; "convert minus 6")]
+    #[test_case(-7 => "-="; "convert minus 7")]
+    #[test_case(-8 => "=2"; "convert minus 8")]
+    #[test_case(-9 => "=1"; "convert minus 9")]
+    #[test_case(-10 => "=0"; "convert minus 10")]
+    #[test_case(-15 => "-20"; "convert minus 15")]
+    #[test_case(-20 => "-10"; "convert minus 20")]
+    #[test_case(-2022 => "-2--1="; "convert minus 2022")]
+    #[test_case(-12345 => "-101110"; "convert minus 12345")]
+    #[test_case(-314159265 => "--=-1---01-20"; "convert minus 314159265")]
+    fn test_to_balanced_quinary(number: i64) -> String {
+        to_balanced_quinary(number)
     }
 }
